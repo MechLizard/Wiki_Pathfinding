7c73f497acea930238f8c381ff289e72dc232fc5
bmansour1 <117964164+bmansour1@users.noreply.github.com>
2024-04-22 19:05:08 -0400

    Update addon.cpp
    Addon copy and change

--- backend/src/addon.cpp
+++ backend/src/addon.cpp
@@ -17,6 +17,18 @@ using namespace std;
 WikiImporter importer;
 unordered_map<wstring, Article *> articles;
 
+
+void LogMessage(const Napi::Env& env, const std::string& message) {
+    Napi::HandleScope scope(env);
+
+    Napi::Object global = env.Global();
+    Napi::Object console = global.Get("console").As<Napi::Object>();
+    Napi::Function log = console.Get("log").As<Napi::Function>();
+
+    log.Call(console, { Napi::String::New(env, message) });
+}
+
+
 // Function to load articles from a file
 void LoadArticles(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
@@ -66,20 +78,24 @@ wstring outputArticles(vector<Article*> &path, time_point<high_resolution_clock>
 Napi::String PerformBothSearches(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
-    std::string origin = info[0].As<Napi::String>();
-    std::string destination = info[1].As<Napi::String>();
-    
-    if (info.Length() < 2 || !origin.IsString() || destination.IsString()) {
+    if (info.Length() < 2 || !info[0].IsString() || !info[1].IsString()) {
         Napi::TypeError::New(env, "Expected two string arguments: origin and destination").ThrowAsJavaScriptException();
         return Napi::String::New(env, "");
     }
 
-
+    // Convert from UTF-8 std::string to UTF-16 std::wstring
+    std::string origin = info[0].As<Napi::String>().Utf8Value();
+    std::string destination = info[1].As<Napi::String>().Utf8Value();
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
     std::wstring originW = converter.from_bytes(origin);
     std::wstring destinationW = converter.from_bytes(destination);
 
-    wstring output;
+    std::wstring output;
+
+    Napi::Global global = env.Global();
+    Napi::Object console = global.Get(originW).As<Napi::Object>();
+    std::wcout << L"OriginW: " << originW << std::endl;
+    std::wcout << L"DestinationW: " << destinationW << std::endl;
 
     // Perform BFS
     auto start = chrono::high_resolution_clock::now();
@@ -101,16 +117,37 @@ Napi::String PerformBothSearches(const Napi::CallbackInfo& info) {
         output += L"DFS: Failed to find a connecting path\n";
     }
 
+    std::wstring debugMessage = L"OriginW: " + originW + L"\nDestinationW: " + destinationW + L"\n";
+    std::wstring output = debugMessage;
+
     // Convert wstring output to string
     std::string outputStr = converter.to_bytes(output);
     return Napi::String::New(env, outputStr);
 }
 
+Napi::Array GetArticleTitles(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::Array titlesArray = Napi::Array::New(env);
+
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    int index = 0;
+    for (const auto& articlePair : articles) {
+        // Convert wstring to UTF-8 string and set in array
+        std::string titleUtf8 = converter.to_bytes(articlePair.first);
+        titlesArray.Set(index++, Napi::String::New(env, titleUtf8));
+    }
+
+    return titlesArray;
+}
+
+
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set("loadArticles", Napi::Function::New(env, LoadArticles));
     exports.Set("performBothSearches", Napi::Function::New(env, PerformBothSearches));
+    exports.Set("getArticleTitles", Napi::Function::New(env, GetArticleTitles));
     return exports;
 }
 
 NODE_API_MODULE(addon, Init)
+
